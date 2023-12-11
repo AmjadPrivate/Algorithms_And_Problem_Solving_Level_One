32a9c8f59f1f4518f3f746c95f4a34b03caaba91
AmjadLap <amjad.sayed.private@gmail.com>
2023-12-11 19:13:28 +0200

    :)

--- SolutionsForTwentyOneToThirty/SolutionsForTwentySixToThirty/SolutionsForThirty/AnotherSolution.cpp
+++ SolutionsForTwentyOneToThirty/SolutionsForTwentySixToThirty/SolutionsForThirty/AnotherSolution.cpp
@@ -6,17 +6,35 @@ int ReadPositiveNumber(string Message)
 {
     int Number;
 
-    cout << Message << endl;
-    cin >> Number;
+
+    do 
+    {
+
+        cout << Message << endl;
+        cin >> Number;
+    
+    }
+    while (Number < 0);
 
     return Number; 
+}
+
+int Factorial (int N)
+{
+    int F = 1;
+
+    for(int Counter = N; Counter >= 1; Counter--)
+    {
+        F = F * Counter;
+    }
+
+    return F;
 }
               
 
 int main()
 {
+    cout << Factorial(ReadPositiveNumber("Please Enter Positive Number: ")) << endl;
     
-    ReadPositiveNumber("Please Enter Positive Number: "); 
-
     return 0;
 }
