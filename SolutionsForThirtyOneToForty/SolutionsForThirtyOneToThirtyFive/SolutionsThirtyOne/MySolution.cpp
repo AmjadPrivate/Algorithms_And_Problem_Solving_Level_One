5678c3049bbedd6f2a905af639837dbf23efe95d
AmjadLap <amjad.sayed.private@gmail.com>
2023-12-11 19:11:50 +0200

    :)

---
+++ SolutionsForThirtyOneToForty/SolutionsForThirtyOneToThirtyFive/SolutionsThirtyOne/MySolution.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int ReadNumber(string Message)
+{
+    int Number;
+
+    cout << Message << endl;
+    cin >> Number;
+
+    return Number;
+}
+
+int Power(int Number, int Power)
+{
+    int Sum = 0;
+
+    for(int Counter = 1; Counter < Power; Counter++)
+    {
+        Sum = Number * Number;
+    }
+
+    return Sum;
+
+}
+
+void PrintResult(int Number)
+{
+    cout << "\n" << Number <<" To The Power Of 2 Is " << Power(Number, 2) << endl; 
+    
+    cout << Number <<" To The Power Of 3 Is " << Power(Number, 3) << endl; 
+    cout << Number <<" To The Power Of 4 Is " << Power(Number, 4) << endl; 
+}
+
+int main()
+{
+    int Number = ReadNumber("Please Enter A Number: ");
+
+    PrintResult(Number);
+ 
+    return 0;
+}
