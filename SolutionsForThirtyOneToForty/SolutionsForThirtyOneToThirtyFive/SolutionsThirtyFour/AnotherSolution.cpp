f0984c432195322ea9448fe4db46d20da5bdc37b
AmjadLap <amjad.sayed.private@gmail.com>
2023-12-14 10:33:12 +0200

    :)

--- SolutionsForThirtyOneToForty/SolutionsForThirtyOneToThirtyFive/SolutionsThirtyFour/AnotherSolution.cpp
+++ SolutionsForThirtyOneToForty/SolutionsForThirtyOneToThirtyFive/SolutionsThirtyFour/AnotherSolution.cpp
@@ -16,14 +16,29 @@ float GetCommisionPercentage(float TotalSales)
 {
     if(TotalSales >= 1000000)
         return 0.01;
-    else if (TotalSales >= 1000000)
-        return
+    else if (TotalSales >= 500000)
+        return 0.02;
+    else if (TotalSales >= 100000)
+        return 0.03;
+    else if (TotalSales >= 500000)
+        return 0.03;
+    else
+      return 0.00;
+
 }
 
+float CalculateToatlCommision(float TotalSales)
+{
+    return GetCommisionPercentage(TotalSales) * TotalSales;
+}
 
 int main()
 {
+    float TotalSales = ReadTotalSales();
 
+    cout << "Commision Percetnage: " << GetCommisionPercentage(TotalSales) << endl;
+
+    cout << "Total Commision: " << CalculateToatlCommision(TotalSales) << endl;
 
 
     return 0;
